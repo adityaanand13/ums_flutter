9e72c8512f49e075fc3ce31aab1235b8ee32e658
Aditya Anand <adityaanand13@live.com>
2020-04-06 04:19:27 +0530

    sample profile page

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -3,9 +3,23 @@ import 'package:flutter/material.dart';
 class Profile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      body: Text("This is your profile"),
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          Image.asset(
+            "images/profile.jpg",
+            height: 600,
+            fit: BoxFit.cover,
+            colorBlendMode: BlendMode.hue,
+            color: Colors.black,
+          ),
+          Expanded(
+              child: Container(
+            height: 300,
+            color: Colors.teal,
+          )),
+        ],
+      ),
     );
   }
-
 }
